57bf346d68b8b4387596d19d6f391f6af85f2b3a
Cyril <79414187+cyrilakafia@users.noreply.github.com>
2023-06-24 11:26:20 +0000

    Fix: Error Handling

--- covidml/faq_contacts.py
+++ covidml/faq_contacts.py
@@ -4,7 +4,7 @@ def faqs():
         {'question': 'Is StormPred free to use?', 'answer': 'Yes. This web app is completely free.'},
         {'question': 'What kind of data do I need to provide to use StormPred?', 'answer': 'No personal data is collected. Users only need to provide data on the compound.'},
         {'question': 'Is StormPred for professionals only?', 'answer': 'No. Anyone can use this web app as long as they have their data.'},
-        {'question': 'How accurate are the predictions made by StormPred?', 'answer': 'The XGBoost and Decision Tree models were able to predict all experimentally validated inhibitors of TLR4 as active with a confidence range of 0.72-1.00. This makes StormPred reliable.'},
+        {'question': 'How accurate are the predictions made by StormPred?', 'answer': 'The XGBoost and Decision Tree models were able to predict all experimentally validated inhibitors of TLR4 as active with a confidence range of 0.83-1.00. This makes StormPred reliable.'},
         {'question': 'How can I provide feedback or report issues with StormPred?', 'answer': 'In case of any feedback or issues, please contact our support team at support@stormpred.com.'}
         # Add more FAQ objects as needed
     ]
