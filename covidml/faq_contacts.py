ab725b642ce957af4db95691bbaabd9f6a65db84
Cyril <kwakucyril@gmail.com>
2024-07-24 13:18:07 -0400

    change stormpred --> aicpred

--- covidml/faq_contacts.py
+++ covidml/faq_contacts.py
@@ -1,11 +1,11 @@
 def faqs():
     faqs_list = [
-        {'question': 'How does StormPred work? ', 'answer': 'StormPred uses machine learning models trained on experimentally-tested TLR4 inhibitors to predict the inhibitive capacity of small anti-inflammatory molecules against the TLR4 receptor.'},
-        {'question': 'Is StormPred free to use?', 'answer': 'Yes. This web app is completely free.'},
-        {'question': 'What kind of data do I need to provide to use StormPred?', 'answer': 'No personal data is collected. Users only need to provide data on the compound.'},
-        {'question': 'Is StormPred for professionals only?', 'answer': 'No. Anyone can use this web app as long as they have their data.'},
-        {'question': 'How accurate are the predictions made by StormPred?', 'answer': 'The XGBoost and Decision Tree models were able to predict all experimentally validated inhibitors of TLR4 as active with a confidence range of 0.83-1.00. This makes StormPred reliable.'},
-        {'question': 'How can I provide feedback or report issues with StormPred?', 'answer': 'In case of any feedback or issues, please contact our support team at support@stormpred.com.'}
+        {'question': 'How does AICPred work? ', 'answer': 'AICPred uses machine learning models trained on experimentally-tested TLR4 inhibitors to predict the inhibitive capacity of small anti-inflammatory molecules against the TLR4 receptor.'},
+        {'question': 'Is AICPred free to use?', 'answer': 'Yes. This web app is completely free.'},
+        {'question': 'What kind of data do I need to provide to use AICPred?', 'answer': 'No personal data is collected. Users only need to provide data on the compound.'},
+        {'question': 'Is AICPred for professionals only?', 'answer': 'No. Anyone can use this web app as long as they have their data.'},
+        {'question': 'How accurate are the predictions made by AICPred?', 'answer': 'The XGBoost and Decision Tree models were able to predict all experimentally validated inhibitors of TLR4 as active with a confidence range of 0.83-1.00. This makes AICPred reliable.'},
+        {'question': 'How can I provide feedback or report issues with AICPred?', 'answer': 'In case of any feedback or issues, please contact our support team at kwakucyril@gmail.com'}
         # Add more FAQ objects as needed
     ]
     
