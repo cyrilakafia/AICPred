89cee758710468e786e1ef7f5ff15b610750a913
Cyril <kwakucyril@gmail.com>
2024-07-28 04:02:01 -0400

    adaboost

--- covidml/forms.py
+++ covidml/forms.py
@@ -5,7 +5,7 @@ from wtforms.validators import DataRequired
 class UploadForm(FlaskForm):
     molecule_id = IntegerField(label='Molecule ID', validators=[DataRequired(message="Please enter a valid integer for the molecule ID.")])
     smiles = StringField(label='SMILES', validators=[DataRequired(message="Please enter a SMILES string.")])
-    model_type = SelectField(label='Algorithm', choices=[('XGBoost', 'XGBoost'), ('Decision Tree', 'Decision Tree')], validators=[DataRequired()])
+    model_type = SelectField(label='Algorithm', choices=[('XGBoost', 'XGBoost'), ('Decision Tree', 'Decision Tree'), ('AdaBoost', 'AdaBoost')], validators=[DataRequired()])
     submit = SubmitField(label='Predict')
 
 
